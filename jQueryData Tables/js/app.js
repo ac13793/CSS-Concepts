58151a0c414268ba7a85f8f9abd791bc0d68e572
Ankit Chaurasia <ankit.chaurasia@ggktech.local>
2017-09-20 18:08:58 +0530

    Added jQuery.SearchHighlight plugin

--- jQueryData Tables/js/app.js
+++ jQueryData Tables/js/app.js
@@ -6,15 +6,40 @@ $(document).ready(function () {
 
 
     // When data is available in object format then defines columns
-    $('#example').DataTable({
+    // $('#example').DataTable({
+    //     "ajax": "data/objects.txt",
+    //     "columns": [
+    //         {"data": "name"},
+    //         {"data": "position"},
+    //         {"data": "office"},
+    //         {"data": "extn"},
+    //         {"data": "start_date"},
+    //         {"data": "salary"}
+    //     ]
+    // });
+
+
+    // Configuration to highlight searched text
+    var table = $('#example').DataTable({
         "ajax": "data/objects.txt",
         "columns": [
-            {"data": "name"},
-            {"data": "position"},
-            {"data": "office"},
-            {"data": "extn"},
-            {"data": "start_date"},
-            {"data": "salary"}
-        ]
+            { "data": "name" },
+            { "data": "position" },
+            { "data": "office" },
+            { "data": "extn" },
+            { "data": "start_date" },
+            { "data": "salary" }
+        ],
+        /*
+        * For enabling search highlight
+        */
+        searchHighlight: true
     });
+
+    table.on('draw', function () {
+        var body = $(table.table().body());
+        body.unhighlight();
+        body.highlight(table.search());
+    });
+
 });
